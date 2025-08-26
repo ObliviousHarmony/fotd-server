ada894c800e0d5bcd12c47159ce890eac31545f1
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2025-08-26 01:24:30 -0700

    Initial Commit
    - Repository Structure: Basic top-level CMake Project to orchestrate the
      building of the various binaries and libraries in the repository.
    - RakNet 3.611: Original source files with a few unused plugins removed
      and a custom CMake project that supports both Windows and Linux builds.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Face of Mankind Server Emulator
+
+This project is a server emulator for the MMORPG "Face of Mankind". It aims to replicate the functionality of the original game servers, allowing players to experience the game in a private server environment.
---
+++ extern/raknet/src/PluginInterface2.cpp
@@ -0,0 +1,39 @@
+/// \file
+///
+/// This file is part of RakNet Copyright 2003 Jenkins Software LLC
+///
+/// Usage of RakNet is subject to the appropriate license agreement.
+
+
+#include "PluginInterface2.h"
+#include "RakPeerInterface.h"
+#include "BitStream.h"
+
+PluginInterface2::PluginInterface2()
+{
+	rakPeerInterface=0;
+}
+PluginInterface2::~PluginInterface2()
+{
+
+}
+void PluginInterface2::SendUnified( const RakNet::BitStream * bitStream, PacketPriority priority, PacketReliability reliability, char orderingChannel, SystemAddress systemAddress, bool broadcast )
+{
+	rakPeerInterface->Send(bitStream, priority, reliability, orderingChannel, systemAddress, broadcast);
+}
+Packet *PluginInterface2::AllocatePacketUnified(unsigned dataSize)
+{
+	return rakPeerInterface->AllocatePacket(dataSize);
+}
+void PluginInterface2::PushBackPacketUnified(Packet *packet, bool pushAtHead)
+{
+	return rakPeerInterface->PushBackPacket(packet, pushAtHead);
+}
+bool PluginInterface2::SendListUnified( char **data, const int *lengths, const int numParameters, PacketPriority priority, PacketReliability reliability, char orderingChannel, SystemAddress systemAddress, bool broadcast )
+{
+	return rakPeerInterface->SendList(data, lengths, numParameters, priority, reliability, orderingChannel, systemAddress, broadcast);
+}
+void PluginInterface2::SetRakPeerInterface( RakPeerInterface *ptr )
+{
+	rakPeerInterface=ptr;
+}
