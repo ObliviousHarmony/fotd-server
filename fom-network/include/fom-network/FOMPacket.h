1b612109e9d03e5dac3ee604f8a1411e64b84d32
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2025-09-24 19:22:08 -0700

    Fixed Code Style

--- fom-network/include/fom-network/FOMPacket.h
+++ fom-network/include/fom-network/FOMPacket.h
@@ -3,10 +3,10 @@
 #include <fom-network/PacketIdentifier.h>
 
 // Include all packet types here.
-#include <fom-network/packets/ReadPacketError.h>
 #include <fom-network/packets/Login.h>
 #include <fom-network/packets/LoginRequest.h>
 #include <fom-network/packets/LoginRequestReturn.h>
+#include <fom-network/packets/ReadPacketError.h>
 
 /**
  * Make sure that we pack the structs the same way that C# does.
