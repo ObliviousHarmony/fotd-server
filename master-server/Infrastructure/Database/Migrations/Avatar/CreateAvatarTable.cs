87cbb6ea4c32231f45925a406f2de8c646609f5a
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2025-12-02 23:31:18 -0800

    Renamed Avatar Table

--- master-server/Infrastructure/Database/Migrations/Avatar/CreateAvatarTable.cs
+++ master-server/Infrastructure/Database/Migrations/Avatar/CreateAvatarTable.cs
@@ -3,13 +3,13 @@ using FOMServer.Master.Extensions;
 
 namespace FOMServer.Master.Infrastructure.Database.Migrations.Avatar
 {
-    [Migration(202509280939, "Creates the `avatar` table.")]
+    [Migration(202509280939, "Creates the `player_avatar` table.")]
     public class CreateAvatarTable : Migration
     {
         public override void Up()
         {
-            Create.Table("avatar")
-                .WithColumn("player_id").AsUnsignedInt().NotNullable().PrimaryKey().ForeignKey("fk_avatar_player", "player", "id")
+            Create.Table("player_avatar")
+                .WithColumn("player_id").AsUnsignedInt().NotNullable().PrimaryKey().ForeignKey("fk_player_avatar_player", "player", "id")
                 .WithColumn("name").AsString(19).NotNullable().Unique()
                 .WithColumn("biography").AsString(510).NotNullable()
                 .WithColumn("faction").AsUnsignedByte().NotNullable()
@@ -22,7 +22,7 @@ namespace FOMServer.Master.Infrastructure.Database.Migrations.Avatar
 
         public override void Down()
         {
-            Delete.Table("avatar");
+            Delete.Table("player_avatar");
         }
     }
 }
