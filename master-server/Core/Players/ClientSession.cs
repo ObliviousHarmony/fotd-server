6f3c8331bf957c3107c2b1889fe6208b0d6afea5
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2026-05-25 14:51:26 -0700

    Added Session World Info to Master

--- master-server/Core/Players/ClientSession.cs
+++ master-server/Core/Players/ClientSession.cs
@@ -1,36 +1,128 @@
+using FOMServer.Shared.Core.Enums;
 using FOMServer.Shared.Core.Packets.Types;
 
 namespace FOMServer.Master.Core.Players
 {
     internal class ClientSession
     {
+        private readonly Lock _syncRoot = new();
+        private WorldID? _pendingWorld;
+
         public ClientSession(NetworkAddress address)
         {
             Address = address;
         }
 
         public NetworkAddress Address { get; }
-        public uint? PlayerID { get; private set; }
-        public Player? Player { get; private set; }
 
-        public bool IsLoggingIn => PlayerID.HasValue && Player is null;
-        public bool IsReady => Player is not null;
+        public uint? PlayerID
+        {
+            get
+            {
+                lock (_syncRoot)
+                    return field;
+            }
+            private set
+            {
+                lock (_syncRoot)
+                    field = value;
+            }
+        }
+
+        public Player? Player
+        {
+            get
+            {
+                lock (_syncRoot)
+                    return field;
+            }
+            private set
+            {
+                lock (_syncRoot)
+                    field = value;
+            }
+        }
+
+        public WorldID? CurrentWorld
+        {
+            get
+            {
+                lock (_syncRoot)
+                    return field;
+            }
+
+            private set
+            {
+                lock (_syncRoot)
+                    field = value;
+            }
+        }
+
+        public bool IsLoggingIn
+        {
+            get
+            {
+                lock (_syncRoot)
+                    return PlayerID.HasValue && Player is null;
+            }
+        }
+
+        public bool IsReady
+        {
+            get
+            {
+                lock (_syncRoot)
+                    return Player is not null;
+            }
+        }
 
         public void BeginLogin(uint playerID)
         {
-            if (PlayerID is not null)
-                throw new InvalidOperationException("Session login already started");
+            lock (_syncRoot)
+            {
+                if (PlayerID is not null)
+                    throw new InvalidOperationException("Session login already started");
 
-            PlayerID = playerID;
+                PlayerID = playerID;
+            }
         }
 
         public void CompleteLogin(Player player)
         {
-            if (player.ID != PlayerID)
-                throw new InvalidOperationException(
-                    $"Player ID {player.ID} does not match the session's login ID {PlayerID}");
+            lock (_syncRoot)
+            {
+                if (player.ID != PlayerID)
+                    throw new InvalidOperationException(
+                        $"Player ID {player.ID} does not match the session's login ID {PlayerID}");
 
-            Player = player;
+                Player = player;
+            }
+        }
+
+        public void BeginWorldTransfer(WorldID world)
+        {
+            if (world == WorldID.MasterServer)
+                throw new ArgumentException("Must use a valid WorldID", nameof(world));
+
+            lock (_syncRoot)
+            {
+                if (_pendingWorld.HasValue)
+                    throw new InvalidOperationException("A world transfer is already in progress");
+
+                _pendingWorld = world;
+            }
+        }
+
+        public void CompleteWorldTransfer()
+        {
+            lock (_syncRoot)
+            {
+                if (!_pendingWorld.HasValue)
+                    throw new InvalidOperationException("There is no world transfer in progress");
+
+                CurrentWorld = _pendingWorld;
+                _pendingWorld = null;
+            }
         }
     }
 }
--- world-server/Core/Players/ClientSession.cs
+++ world-server/Core/Players/ClientSession.cs
@@ -1,36 +1,20 @@
+using FOMServer.Shared.Core.Enums;
 using FOMServer.Shared.Core.Packets.Types;
 
 namespace FOMServer.World.Core.Players
 {
     internal class ClientSession
     {
-        public ClientSession(NetworkAddress address)
+        private readonly Lock _syncRoot = new();
+
+        public ClientSession(NetworkAddress address, uint playerID)
         {
             Address = address;
-        }
-
-        public NetworkAddress Address { get; }
-        public uint? PlayerID { get; private set; }
-        public Player? Player { get; private set; }
-
-        public bool IsLoggingIn => PlayerID.HasValue && Player is null;
-        public bool IsReady => Player is not null;
-
-        public void BeginLogin(uint playerID)
-        {
-            if (PlayerID.HasValue)
-                throw new InvalidOperationException("Session login already started");
-
             PlayerID = playerID;
         }
 
-        public void CompleteLogin(Player player)
-        {
-            if (player.ID != PlayerID)
-                throw new InvalidOperationException(
-                    $"Player ID {player.ID} does not match the session's login ID {PlayerID}");
+        public NetworkAddress Address { get; }
 
-            Player = player;
-        }
+        public uint PlayerID { get; }
     }
 }
