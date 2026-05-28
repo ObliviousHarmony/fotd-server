45a48649e222a3ba8258fdab90c4bfb70744847e
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2026-05-27 17:38:37 -0700

    Prevent Crazed Conditional Ternaries

--- master-server/Application/Handlers/LoginRequestHandler.cs
+++ master-server/Application/Handlers/LoginRequestHandler.cs
@@ -39,13 +39,22 @@ namespace FOMServer.Master.Application.Handlers
             }
 
             var player = _accountRepository.GetByUsername(p.Username);
-            rData.Status = player is null
-                ? LoginRequestReturn.StatusCode.Invalid
-                : player.logged_in
-                    ? LoginRequestReturn.StatusCode.AlreadyLoggedIn
-                    : p.ClientVersion != ServerConstants.ClientVersion
-                    ? LoginRequestReturn.StatusCode.VersionMismatch
-                    : LoginRequestReturn.StatusCode.Success;
+            if (player is null)
+            {
+                rData.Status = LoginRequestReturn.StatusCode.Invalid;
+            }
+            else if (player.logged_in)
+            {
+                rData.Status = LoginRequestReturn.StatusCode.AlreadyLoggedIn;
+            }
+            else if (p.ClientVersion != ServerConstants.ClientVersion)
+            {
+                rData.Status = LoginRequestReturn.StatusCode.VersionMismatch;
+            }
+            else
+            {
+                rData.Status = LoginRequestReturn.StatusCode.Success;
+            }
 
             _clientPacketSender.Send(response.Build());
         }
