c1f22227cc8525f7e156ac16dd419f5578a28a9f
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2026-07-11 00:57:23 -0700

    Documented Remaining Container Types

--- server-shared/Core/Enums/ItemContainerType.cs
+++ server-shared/Core/Enums/ItemContainerType.cs
@@ -7,9 +7,18 @@ namespace FOMServer.Shared.Core.Enums
         Inventory = 1, // ITEM_CONTAINER_INVENTORY
         Equipment = 2, // ITEM_CONTAINER_EQUIPMENT
         Weapons = 3, // ITEM_CONTAINER_WEAPONS
-
+        NanomachineAugmentations = 4, // ITEM_CONTAINER_NANOMACHINE_AUGMENTATIONS
         Quickslots = 5, // ITEM_CONTAINER_QUICKSLOTS
-
+        Storage = 6, // ITEM_CONTAINER_STORAGE
+        ActiveConsumables = 7, // ITEM_CONTAINER_ACTIVE_CONSUMABLES
         Destroy = 8, // ITEM_CONTAINER_DESTROY
+
+        Loot = 11, // ITEM_CONTAINER_LOOT
+
+        TransportStorage = 13, // ITEM_CONTAINER_TRANSPORT_STORAGE
+
+        TransportBuyback = 17, // ITEM_CONTAINER_TRANSPORT_BUYBACK
+
+        NUM_ITEM_CONTAINERS = 18
     }
 }
